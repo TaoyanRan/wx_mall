37d97c39f60ffbfb2551bc8a401187683f4f7592
@抱枕头的懒猪 <1242599243@qq.com>
2016-10-13 19:11:50 +0800

    新增 我的 栏目下首页

--- mall/my/index.js
+++ mall/my/index.js
@@ -1,7 +1,19 @@
 //获取应用实例
+
 var app = getApp();
-
 Page({
-    data: {}
+    data: {
+        userInfo : {}
+    },
+    onLoad: function () {
+        var _this = this;
+        app.getUserInfo(function(userInfo){
 
+            _this.setData({
+                userInfo : userInfo
+            })
+
+        })
+
+    }
 });
